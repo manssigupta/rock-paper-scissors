44ef00d1ea44f8e20ed3c59d9342922a2a4fae1d
Mansi Gupta <150427241+manssigupta@users.noreply.github.com>
2024-09-25 14:24:43 +0530

    To get user choice

--- rps.js
+++ rps.js
@@ -1,7 +1,15 @@
 
+
 function getComputerChoice(){
 
     const choice=["Rock","Paper","Scissor"];
     const randomElement=choice[Math.floor(Math.random()*choice.length)];
     return randomElement;
 }
+console.log(getComputerChoice());
+
+function getHumanChoice(){
+        let input = prompt("Enter choice:");
+        return input;
+}
+console.log(getHumanChoice());
