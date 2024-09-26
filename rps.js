744b2384dea448ec13346ba13927a2a32a3f8590
Mansi Gupta <150427241+manssigupta@users.noreply.github.com>
2024-09-26 22:28:24 +0530

    Add logic to play entire game

--- rps.js
+++ rps.js
@@ -2,17 +2,90 @@
 
 function getComputerChoice(){
 
-    const choice=["Rock","Paper","Scissor"];
-    const randomElement=choice[Math.floor(Math.random()*choice.length)];
-    return randomElement;
+    let computerChoice = Math.floor(Math.random()*3);
+    if(computerChoice == 0){
+        return "Rock";
+    }else if(computerChoice == 1){
+        return "Paper";
+    }
+    else{
+        return "Scissor";
+    }
 }
-console.log(getComputerChoice());
+
 
 function getHumanChoice(){
-        let input = prompt("Enter choice:");
-        return input;
+        let humanChoice = prompt("Enter choice:");
+        while(true){
+            if(humanChoice === null){
+                console.log("ok we will play next time");
+                return null;
+            }
+            humanChoice= humanChoice.toLowerCase();
+            if(humanChoice === "rock"){
+                return "Rock";
+            }else if(humanChoice ==="paper"){
+                return "Paper";
+            }else if(humanChoice === "scissor"){
+                return "Scissors";
+            }
+            else{
+                humanChoice = prompt("invalid!");
+            }
+        }
 }
-console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+
+
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        return "tie";
+    }
+    else if(humanChoice === "Rock" && computerChoice==="Scissor"||humanChoice=== "Paper" && computerChoice === "Rock" || humanChoice === "Scissor" && computerChoice === "Paper"){
+        return "Human";
+    }
+    else{
+        return "Computer";
+    }
+}
+
+function playGame(){
+    console.log("Time to play!");
+    let humanPoints = 0;
+    let computerPoints = 0;
+    while(humanPoints < 3 && computerPoints < 3){
+        let humanChoice = getHumanChoice();
+        if(humanChoice === null){
+            return;
+        }
+        let computerChoice = getComputerChoice();
+        console.log("You chose"+humanChoice+" and computer chose "+computerChoice);
+        let winner = playRound(humanChoice,computerChoice);
+        if(winner === "Tie"){
+            console.log("its a tie");
+        }else if(winner === "Human"){
+            console.log("you win");
+            humanPoints++;
+        }
+        else if(winner === "Computer"){
+            console.log("you lose!");
+            computerPoints++;
+        }
+        console.log("Your points: "+ humanPoints);
+        console.log("Computer points: "+ computerPoints);
+    }
+    if(humanPoints === 3){
+        console.log("you won at last");
+    }
+    else{
+        console.log("computer won at last");
+    }
+   
+        
+    
+    
+}
+playGame();
+
+
